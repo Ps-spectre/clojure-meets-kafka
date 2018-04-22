6116e0347903b763094e172d216b44e575c4d50a
Andrew Kishchenko <psspectre@gmail.com>
2018-04-23 01:33:57 +0300

    wip: get messages from topic

--- src/clojure_meets_kafka/filters.clj
+++ src/clojure_meets_kafka/filters.clj
@@ -4,5 +4,7 @@
 
 (defn add-filter [topic-name q]
   (println "OFFSET: " (kafka/get-offset-topic topic-name))
+  (println "msg: " (kafka/get-messages topic-name 0))
   (println "add-filter" topic-name q))
+
 
--- src/clojure_meets_kafka/kafka.clj
+++ src/clojure_meets_kafka/kafka.clj
@@ -19,3 +19,17 @@
       (assign-partitions! c topic-partitions)
       (seek-to-end-offset! c topic-partitions)
       (next-offset c (get topic-partitions 0)))))
+
+(defn get-messages [topic-name offset]
+  (let [cc {:bootstrap.servers config/kafka-brokers
+            :auto.offset.reset :none}
+        key-deserializer (deserializers/string-deserializer)
+        value-deserializer (deserializers/string-deserializer)
+        options (cd/make-default-consumer-options)
+        topic topic-name
+        topic-partitions [{:topic topic :partition 0}]]
+    (with-open [c (consumer/make-consumer cc key-deserializer value-deserializer options)]
+      (assign-partitions! c topic-partitions)
+      (seek-to-offset! c (get topic-partitions 0) offset)
+      (let [ cr (poll! c)]
+        (->> (into [] cr) (map #(get % :value)) )))))
